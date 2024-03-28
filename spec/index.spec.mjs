5b45dae65ea438438ad4cdeaae5123518a4e3cee
Blanca Bigeriego <blancabcbt@gmail.com>
2024-03-28 20:43:03 +0100

    Feat: add kata structure

---
+++ spec/index.spec.mjs
@@ -0,0 +1,8 @@
+import { LeapYear } from "../index.mjs";
+
+describe('LEAP YEAR KATA', () => {
+    it('should create', () => {
+        const leapyear = new LeapYear();
+        expect(leapyear).toBeDefined();
+    })
+})
